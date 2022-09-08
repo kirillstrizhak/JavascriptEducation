8962371183865adf324414110fec485238e9d900
Kirill Strizhak <holdup0192@mail.ru>
2022-09-08 16:27:40 +0300

    5. Пятое ДЗ Final

--- script.js
+++ script.js
@@ -208,7 +208,7 @@ let cart = [
     ['Кроссовки', 2, 5000]
 ];
 
-function countCartPrice() {
+function quantityCartPrice() {
     for (i = 0; i < cart.length; i++) {
         cartPrice += cart[i][2] * cart[i][1]
     };
@@ -216,7 +216,7 @@ function countCartPrice() {
     return cartPrice;
 }
 
-countCartPrice();*/
+quantityCartPrice();*/
 
 //Задача 3
 
@@ -278,82 +278,146 @@ getNumberProperties()*/
 let cart = {    //Создаём объект корзины со свойством массива и методом - функцией вычисления стоимости
     products: [{       //Создаём массив товаров
         name: 'Толстовка',
-        count: 4,
+        quantity: 4,
         price: 2000
     },
     {
         name: 'Шорты',
-        count: 3,
+        quantity: 3,
         price: 1000
     },
     {
         name: 'Брюки',
-        count: 3,
+        quantity: 3,
         price: 2500
     },
     {
         name: 'Кроссовки',
-        count: 2,
+        quantity: 2,
         price: 5000
     }
     ],
-    countCartPrice: function () {
+    quantityCartPrice: function () {
         let cartPrice = 0;
         for (let i = 0; i < cart.products.length; i++) {
-            cartPrice += cart.products[i].count * cart.products[i].price;
+            cartPrice += cart.products[i].quantity * cart.products[i].price;
         };
         console.log(`Общая цена корзины ${cartPrice}`)
         return cartPrice
     }
 }
 
-cart.countCartPrice() //Выводим полученное значение*/
+cart.quantityCartPrice() //Выводим полученное значение*/
 
 //Домашнее задание 5
 
-let block = document.querySelector('#root')
+//Задача 1
 
-console.log(block)
+// ! Чтобы функция сработала и показала результат, раскомментируйте её вызов !
 
-function createChessBoard(parent, cols, rows) {     //Цикл создания строк
+let block = document.querySelector('#chess')
+function createChessBoard(parent) {
+    let variation = 1
     let table = document.createElement('table');
-    let j = 1
-    for (let i = 0; i < rows; i++) {
+    let letters = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', '']
+    let numbers = ['', '1', '2', '3', '4', '5', '6', '7', '8', '']
+    let reverseNumbers = numbers.reverse()
+    for (let i = 0; i < 10; i++) {
         let tr = document.createElement('tr');
         table.appendChild(tr);
 
-        for (let i = 0; i < cols / rows; i++) {       //Цикл чередования переменной, от которой будет зависеть цвет строк
-            if (j == 1) {
-                j--;
-                console.log(j);
+        for (let i = 0; i < 1; i++) {       //Цикл чередования переменной, от которой будет зависеть цвет строк
+            if (variation == 1) {
+                variation--;
                 tr.className = 'blackCell';
             } else {
-                j++;
-                console.log(j);
+                variation++;
                 tr.className = 'whiteCell';
             }
         };
 
-        for (let i = 0; i < cols; i++) {            //Цикл создания столбцов      
+        for (let j = 0; j < 10; j++) {            //Цикл создания столбцов      
             let td = document.createElement('td');
-            tr.appendChild(td);
+            tr.append(td);
+            td.innerHTML = (j + 1) + i
 
-            for (let i = 0; i < cols / rows; i++) {   //Цикл чередования переменной, от которой будет зависеть цвет столбцов
-                if (j == 1) {
-                    j--;
-                    console.log(j);
+            for (let i = 0; i < 1; i++) {       //Цикл чередования переменной, от которой будет зависеть цвет строк
+                if (variation == 1) {
+                    variation--;
                     td.className = 'blackCell';
                 } else {
-                    j++;
-                    console.log(j);
+                    variation++;
                     td.className = 'whiteCell';
                 }
+            };
+
+            if (i == 9 && 0 <= j <= 9) {
+                td.innerHTML = letters[j]
+            } else if (i == 0 && 0 <= j <= 9) {
+                td.innerHTML = letters[j]
+            } else if (i <= 8 && j == 0) {
+                td.innerHTML = reverseNumbers[i]
+            } else if (i <= 8 && j == 9) {
+                td.innerHTML = reverseNumbers[i]
+            } else {
+                td.innerHTML = ''
             }
         };
+
         parent.appendChild(table);
+    }
+    return table
+}
+//createChessBoard(block);
+
+//Задача 2
+
+const cart = {
+
+    products: [],
+
+    countCartPrice: function () {
+        let cartPrice = 0;
+        for (let i = 0; i < this.products.length; i++) {
+            cartPrice += this.products[i].quantity * this.products[i].price;
+        };
+        return cartPrice
+    },
+
+    countProductsQuantity: function () {
+        let productsQuantity = 0
+        for (let i = 0; i < this.products.length; i++) {
+            productsQuantity = this.products[i].quantity * this.products.length
+            console.log(`В корзине: ${productsQuantity} товаров на сумму ${this.countCartPrice()}.`)
+            return productsQuantity
+        }
+    },
+
+    isCartEmpty: function () {
+        if (this.products.length === 0) {
+            console.log('Корзина пуста.')
+        } else {
+            this.countProductsQuantity()
+        }
     }
 }
 
-createChessBoard(block, 10, 10);
+function Product(productName, quantity, price) {
+    this.productName = productName
+    this.quantity = quantity
+    this.price = price
+}
 
+cart.products.push(new Product('IPhone XR', 4, 45999))
+cart.products.push(new Product('IPhone 11 Pro', 2, 65299))
+cart.products.push(new Product('IPhone 12 Pro MAX', 5, 86529))
 
+function createCart() {
+    let cartBlock = document.querySelector('#cart')
+    let cart = document.createElement('div')
+    cart.className = 'cart'
+    cartBlock.append(cart)
+}
+
+cart.isCartEmpty()
+createCart()
