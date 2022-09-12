c7db24a57541aebec60feccf0284d5f32775f0c5
Kirill Strizhak <holdup0192@mail.ru>
2022-09-12 17:09:41 +0300

    7. Седьмое ДЗ v1

--- script.js
+++ script.js
@@ -7,38 +7,17 @@ const product = {
     productRender(item) {
         return `
     <tr>
-    <td>${item.number}</td>
-    <td>${item.name}</td>
-    <td>${item.quantity}</td>
-    <td>${item.price}</td>
+    <td><img class = "itemImg" src = "${item.img}"></img></td>
+            <td>${item.name}</td>
+            <td>${item.quantity}</td>
+            <td>${item.price}</td>
     </tr>`
     }
 }
 
-const cartBlock = document.querySelector('#cart')
-
 const cart = {
 
-    products: [
-        {
-            number: 1,
-            name: 'Ручка',
-            quantity: 5,
-            price: 25
-        },
-        {
-            number: 2,
-            name: 'Карандаш',
-            quantity: 7,
-            price: 20
-        },
-        {
-            number: 3,
-            name: 'Блокнот',
-            quantity: 10,
-            price: 35
-        }
-    ],
+    products: [],
 
     countCartPrice() {
         let cartPrice = 0;
@@ -53,6 +32,17 @@ const cart = {
 
     },
 
+    addToCart(product) {
+        const existProduct = this.products.find(item => item.id == product.id)
+        if (existProduct) {
+            product.quantity++
+        } else {
+            product.quantity = 1
+            cart.products.push(product)
+        }
+        cart.cartRender()
+    },
+
     clearCart() {
         this.products = []
         this.cartRender()
@@ -63,18 +53,14 @@ const cart = {
     },
 
     cartRender() {
+        const cartBlock = document.querySelector('#cart')
+        cartBlock.innerHTML = ''
         let cartTable = document.createElement('table')
-        cartTable.className = 'cartTable'
+        cartTable.className = 'productsTable'
         cartBlock.append(cartTable)
-        cartBlock.insertAdjacentHTML('afterbegin', '')
+
         if (cart.products.length > 0) {
-            cartTable.insertAdjacentHTML('afterbegin', `
-    <tr class = "cartTabHead">
-    <td>*</td>
-    <td>Название</td>
-    <td>Количество</td>
-    <td>Цена/шт.</td>
-    </tr>`)
+
             cartBlock.insertAdjacentHTML('beforeend', `<p>В корзине ${this.countProductsQuantity()} товаров общей стоимостью ${this.countCartPrice()}</p>`)
 
             let clearBtn = document.createElement('button')
@@ -84,12 +70,87 @@ const cart = {
             clearBtn.addEventListener('click', (e) => this.clearCart())
 
             for (let i = 0; i < this.products.length; i++) {
-                cartTable.insertAdjacentHTML('beforeend', product.productRender(this.products[i]))
+                cartTable.insertAdjacentHTML('afterbegin', `${product.productRender(this.products[i])}`)
             }
+            cartTable.insertAdjacentHTML('afterbegin', `
+        <tr class = "productsTabHead">
+        <td></td>
+        <td>Название</td>
+        <td>Количество</td>
+        <td>Цена/шт.</td>
+        <td></td>
+        </tr>`)
         } else {
             cartBlock.innerHTML = '<p>Корзина пуста.</p>'
         }
     }
 }
 
+const catalog = {
+    products: [
+        {
+            id: 1,
+            name: 'IPhone XR',
+            quantity: 1,
+            img: 'img/iphonexr.jpg',
+            price: 45590
+        },
+        {
+            id: 2,
+            name: 'Iphone 11 Pro MAX',
+            quantity: 1,
+            img: 'img/iphone11.jpg',
+            price: 56999
+        },
+        {
+            id: 3,
+            name: 'IPhone 12 Pro',
+            quantity: 1,
+            img: 'img/iphone12.jpg',
+            price: 86229
+        },
+    ],
+
+    catalogInit() {
+        this.catalogRender()
+    },
+
+    catalogRender() {
+        const catalogBlock = document.querySelector('#catalog')
+        catalogBlock.insertAdjacentHTML('afterbegin', 'Каталог товаров')
+        let catalogTable = document.createElement('table')
+        catalogTable.className = 'productsTable'
+        catalogBlock.append(catalogTable)
+        catalogTable.insertAdjacentHTML('afterbegin', `
+        <tr class = "productsTabHead catalogTab">
+        <td></td>
+        <td>Название</td>
+        <td>Наличие</td>
+        <td>Цена/шт.</td>
+        <td></td>
+        </tr>`)
+
+        for (let i = 0; i < this.products.length; i++) {
+            catalogTable.insertAdjacentHTML('beforeend', `<tr>
+            <td><img class = "itemImg" src = "${this.products[i].img}"></img></td>
+            <td>${this.products[i].name}</td>
+            <td>Есть в наличии</td>
+            <td>${this.products[i].price}</td>
+            <td><button id = buy-btn class = "buyButton item${(i + 1)}">Купить</button></td>
+            </tr>`)
+        }
+
+        let buyButtonXR = document.querySelector('.item1')
+        buyButtonXR.addEventListener('click', (e) => cart.addToCart(this.products[0]))
+
+        let buyButton11 = document.querySelector('.item2')
+        buyButton11.addEventListener('click', (e) => cart.addToCart(this.products[1]))
+
+        let buyButton12 = document.querySelector('.item3')
+        buyButton12.addEventListener('click', (e) => cart.addToCart(this.products[2]))
+
+    }
+}
+
+catalog.catalogInit()
 cart.cartInit()
